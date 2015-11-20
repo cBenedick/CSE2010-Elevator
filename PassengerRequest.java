25650fb87da197af2488cc3b433cee085b657e16
cBenedick <caleb.benedick@my.fit.edu>
2015-11-20 16:18:07 -0500

    Update PassengerRequest.java

--- PassengerRequest.java
+++ PassengerRequest.java
@@ -2,7 +2,7 @@ package project;
 
 import java.sql.Time;
 
-public class PassengerRequest {
+public class PassengerRequest implements Comparable<PassengerRequest>{
 	
 	public Time getTimePressedButton() {
 		return time_pressed_button;
@@ -28,17 +28,31 @@ public class PassengerRequest {
 	public void setWeight(int weight) {
 		this.weight = weight;
 	}
+	public boolean getUp() {
+        return up;
+    }
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
 	private Time time_pressed_button;  // time when the button was pressed
 	private int floor_from; // the floor from which the elevator was called
 	private int floor_to; // the floor where the passenger is headed to
 	private int weight; // weight, in pounds
-	public PassengerRequest(Time time_pressed_button, int floor_from, int floor_to, int weight) {
+	private boolean up;
+
+	public PassengerRequest(Time time_pressed_button, int floor_from, int floor_to, int weight, boolean up) {
 
 		this.time_pressed_button = time_pressed_button;
 		this.floor_from = floor_from;
 		this.floor_to = floor_to;
 		this.weight = weight;
+		this.up = up;
 	}
-	
-	
+
+    @Override
+    public int compareTo(PassengerRequest other) {
+        return this.getTimePressedButton().compareTo(other.getTimePressedButton());
+    }
+    
 }
